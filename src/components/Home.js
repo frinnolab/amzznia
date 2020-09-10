f5a620781ad956923c407554a5d32a8615e565ff
frinno_codes <dev.frinno@gmail.com>
2020-09-11 02:04:18 +0300

    Homepage Done.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../design/Home.css';
 import HomeBanner from '../assets/home_banner.jpg';
+import Product from '../components/Product.js';
 
 
 const Home = () => {
@@ -15,20 +16,27 @@ const Home = () => {
 
                 <div className='home_product_row'>
                     {/**Product*/}
+                    <Product title="Red Vans" price={29.99} rating={3} image={require('../assets/prod_00.jpg')}/>
                     {/**Product*/}
+                    <Product title="Coco Oil Body Lotion" price={17.99} rating={2} image={require('../assets/prod_01.jpg')}/>
                 </div>
 
                 
                 <div className='home_product_row'>
                     {/**Product*/}
+                    <Product title="Wooden Stand" price={5.99} rating={4} image={require('../assets/prod_03.jpg')}/>
+
                     {/**Product*/}
+                    <Product title="Channel Perfume" price={45.99} rating={4} image={require('../assets/prod_04.jpg')}/>
+
                     {/**Product*/}
+                    <Product title="Red Chrome Nike" price={75.99} rating={5} image={require('../assets/prod_05.jpg')}/>
                 </div>
 
                 
                 <div className='home_product_row'>
                     {/**Product*/}
-                    
+                    <Product title="Clinique Red Lipstick" price={4.99} rating={3} image={require('../assets/prod_06.jpg')}/>
                 </div>
            </div>
         </div>
---
+++ src/components/Product.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import '../design/Product.css';
+
+function Product({id, title, price,image, rating}) {
+    return (
+        <div className='product'>
+            <div className='product_info'>
+                <p className='product_title'>
+                    {title}
+                </p>
+
+                <p className='product_price'>
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </p>
+
+                <div className='product_rating'>
+                    {Array(rating)
+                        .fill()
+                        .map((_,i)=>(
+                            <p>🌟</p>
+                        ))}
+                </div>
+
+            </div>
+            <img className='product_img' 
+            src={image} 
+            alt="Product Img"/>
+
+            <button className='product_cart_btn'>Add To Cart</button>
+        </div>
+    )
+}
+
+export default Product
